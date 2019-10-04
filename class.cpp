c5737c4689976fe244a4d7f7adaad0968da110cf
pepe3236 <mepsadecv@outlook.com>
2019-10-04 09:04:05 -0500

    se añadio perimetro

--- class.cpp
+++ class.cpp
@@ -1,1 +1,46 @@
-include
+#include <iostream>
+#include <conio.h>
+#include <stdio>
+
+using namespace std;
+
+class Recatangulo()
+{
+	
+	//Atributos
+	private:
+		
+		float largo, ancho;
+		
+	//Metodos
+	public:
+		void perimetr();
+		void area();
+	
+	
+}
+
+Rectangulo::Rectangulo(float opc_largo, float opc_ancho)
+{
+    largo = opc_largo;
+    ancho = opc_ancho;
+}
+
+void Rectangulo::perimetro()
+{
+    float opc_perimetro;
+
+    opc_perimetro = (2 * largo) + (2 * ancho);
+
+    cout<<"El perimetro es"<<opc_perimetro<<endl;
+}
+
+
+
+int main(){
+	
+	
+	
+	getch();
+	return 0;
+    }
