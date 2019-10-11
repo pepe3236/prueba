78e7663687aa0186242e9f2dd87e5e38b8fb0ffb
pepe3236 <mepsadecv@outlook.com>
2019-10-11 08:03:26 -0500

    El programa terminado

--- class.cpp
+++ class.cpp
@@ -1,59 +1,29 @@
 #include <iostream>
 #include <conio.h>
-#include <stdio>
+#include <stdlib.h>
 
 using namespace std;
 
 class Recatangulo()
 {
+private:
+
+float baseC, alturaC;
+
+public:
+
+
+
+
+
+
+
+}
 	
-	//Atributos
-	private:
-		
-		float largo, ancho;
-		
-	//Metodos
-	public:
-		void perimetr();
-		void area();
 	
-}
-
-Rectangulo::Rectangulo(float opc_largo, float opc_ancho)
-{
-    largo = opc_largo;
-    ancho = opc_ancho;
-}
-
-void Rectangulo::perimetro()
-{
-    float opc_perimetro;
-
-    opc_perimetro = (2 * largo) + (2 * ancho);
-
-    cout<<"El perimetro es"<<opc_perimetro<<endl;
-}
-
-void Rectangulo::area()
-{
-    float opc_area;
-
-    opc_area = largo * ancho;
-
-    cout<<"El area es: "<<apc_area<<endl;
-}
-
 int main()
 {
-//crear un objeto es igual a  = instanciar una clase//
 
-//instanciar clase o crear objeto//
-    Rectangulo rec(5, 6);
-
-    //objeto mandando llamar el metodo de la clase//
-	rec.perimetro();
-    rec.area();
-	
 
 	getch();
 	return 0;
--- class2.cpp
+++ class2.cpp
@@ -6,21 +6,48 @@ using namespace std;
 class Person
 {
 
-private
+private:
     int age;
     string name; 
 
-public
+public:
 
-    Person(int, string)
+    Person(int, string);
     void read();
     void eat();
     void run();
+};
+
+Person::Person(int _age, string _name)
+{
+age = _age;
+name = _name;
+
 }
 
+void Person::read()
+{
+cout<<"Hola soy " <<name<<" y estoyleyendo y tengo "<<age<<" años"<<endl;
 
+}
+
+void Person::eat()
+{
+    cout<<"Mi nombre es "<<name<<" me gusta comer sushi"<<endl;
+
+}
+void Person::run()
+{
+cout<<"Mis amigos e dicen"<<name<<" y corro un maraton de 10 Km"<<endl;
+
+}
 
 int main(){
+
+Person a = Person(22,"Jose");
+a.read();
+a.eat();
+a.run();
 
 
 getch();
