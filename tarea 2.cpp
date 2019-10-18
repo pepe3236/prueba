1456706c772011e273b959c6b353eaa1118d5af6
pepe3236 <mepsadecv@outlook.com>
2019-10-17 22:46:50 -0500

    Tarea 2

---
+++ tarea 2.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+class Date
+{
+	private:
+		string day[2],  month[2], year[4];
+	public:
+		Date(string, string, string);
+		void show();
+
+};
+
+Date::Date(string _day,string _month, string _year)
+{
+    day[2] = _day[2];
+    month[2] = _month[2];
+    year[4] = _year[4];
+
+}
+
+
+
+void Date::show()
+{
+	cout<<"Ingrese el dia, mes y año en ese orden."<<endl;
+	
+	for(int i=0;i<2;i++){
+		cout<<"Digite el dia."<<endl;
+		cin>>day[i];
+	}
+	for(int j=0; j<2; j++){
+		cout<<"Digite el mes."<<endl;
+		cin>>month[j];
+	}
+	for(int p=0; p<4; p++){
+		cout<<"Digite el anio."<<endl;
+		cin>>year[p];
+	}
+	
+	cout<<"La fecha de hoy es "<<day[2]<<"/"<<month[2]<<"/"<<year[4]<<endl;
+
+ 
+}
+
+int main(){
+
+	Date today(string day ,string month ,string year);
+	today.show();
+	
+	getch();
+	    return 0;
+}
